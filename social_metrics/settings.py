57aa004f372118fec5bc7ee5ba6fa36fe1f234b4
kapphire <ken.sapphire87@yahoo.com>
2019-06-22 04:36:41 +1200

    smtp changed

--- social_metrics/settings.py
+++ social_metrics/settings.py
@@ -172,9 +172,11 @@ LOGIN_REDIRECT_URL = '/analytic/'
 
 # EMAIL configurations
 EMAIL_USE_TLS = True
-EMAIL_BACKEND = 'django_ses.SESBackend'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django_ses.SESBackend'
 # EMAIL_HOST = 'email-smtp.us-east-1.amazonaws.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'warrior.coder999@gmail.com'
 EMAIL_HOST_PASSWORD = 'Sapphire@123'
