e92ad054f2c6517af0991dccd5764fbc0eaa613c
kapphire <ken.sapphire87@yahoo.com>
2019-06-22 04:41:46 +1200

    static changed

--- social_metrics/settings.py
+++ social_metrics/settings.py
@@ -145,7 +145,7 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
