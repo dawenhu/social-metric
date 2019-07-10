66dbaa7c356be2772e6863a8d1316e6e64a63010
kapphire <ken.sapphire87@yahoo.com>
2019-07-10 17:00:33 +1200

    patchdash.com added

--- social_metrics/settings.py
+++ social_metrics/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'q44-0ew83de5+tmdau^h()rdd@&^o6sil(c0swm1&b*gsx_kir'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['54.91.114.213', 'localhost']
+ALLOWED_HOSTS = ['54.91.114.213', 'localhost', 'patchdash.com']
 
 
 # Application definition
